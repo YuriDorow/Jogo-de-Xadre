5cf8238db3e8c96fcc56d4162d47c68ee48525d6
Yuri Dorow <yuridorow@gmail.com>
2022-12-07 23:49:39 -0300

    Comecando a colocar pecas no tabuleiro

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -5,12 +5,18 @@ namespace xadrez_console
 {
     internal class Tela
     {
-        public static void imprimirTabuleiro(Tabuleiro tab){
-            for (int i = 0; i < tab.linhas; i++){
-                for (int j = 0; j < tab.colunas; j++){
-                    if (tab.peca(i, j) == null){
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (tab.peca(i, j) == null)
+                    {
                         Console.Write("- ");
-                    }else{
+                    }
+                    else
+                    {
                         Console.Write(tab.peca(i, j) + " ");
                     }
                 }
